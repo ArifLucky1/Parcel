2b980cdffd97a7e04489de398f8e95726e6dbf12
Mohd Arif <ariflucky1@gmail.com>
2026-09-05 22:34:49 +0530

    creates tables

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,20 @@
-import pkg, { Client } from 'pg';
+import pkg, { Client } from "pg";
 const { client } = pkg;
 
 const database = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+try {
+  await database.connect();
+  console.log("Connected to the Database successfully");
+} catch (error) {
+  console.error("Database connection failed:", error);
+  process.exit(1);
+}
+
+export default database;
