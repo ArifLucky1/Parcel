27c8e8b937b02cf4fdf5f180309748f8030fb919
Mohd Arif <ariflucky1@gmail.com>
2026-09-05 18:49:02 +0530

    setting up Database

---
+++ server/database/db.js
@@ -0,0 +1,10 @@
+import pkg, { Client } from 'pg';
+const { client } = pkg;
+
+const database = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+})
