e689e741e8fb02bbb998f9b1f234030c703fae1c
Mohd Arif <ariflucky1@gmail.com>
2026-09-07 23:58:44 +0530

    Middlewares for error handling

---
+++ server/middlewares/errorMiddleware.js
@@ -0,0 +1,32 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+
+class ErrorHandler extends Error {
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+
+export const errorMiddleware = (err, req, res, next) => {
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    if(err.code === 11000){
+        const message = `Duplicate field value entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    if(err.name = "JsonWebTokenError"){
+        const message = "JSON Web Token is Invalid, try again";
+        err = new ErrorHandler(message, 400);
+    }
+
+    if(err.name = "TokenExpiredError"){
+        const message = "JSON Web Token has expired, try again";
+        err = new ErrorHandler(message, 400);
+    }
+};
+
+
+export default ErrorHandler;
