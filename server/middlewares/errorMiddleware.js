0fcd5c0e424019f0bf064ef54af893edcd995e82
Mohd Arif <ariflucky1@gmail.com>
2026-09-09 23:44:28 +0530

    authController login

--- server/middlewares/errorMiddleware.js
+++ server/middlewares/errorMiddleware.js
@@ -11,8 +11,6 @@ export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
-    console.log(err);
-
   if (err.code === 11000) {
     const message = `Duplicate field value entered`;
     err = new ErrorHandler(message, 400);
