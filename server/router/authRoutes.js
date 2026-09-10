e7b67dbb411c9ee9e3857abdf6d9ff0f7ca9e562
Mohd Arif <ariflucky1@gmail.com>
2026-09-10 22:32:38 +0530

    user authenticated.

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -5,12 +5,13 @@ import {
   logout,
   register,
 } from "../controllers/authcontroller.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.get("/logout", isAuthenticated, logout);
 router.get("/me", getUser);
 
 export default router;
