20af56e78dbc6267b30160bbb29772a76c347417
Mohd Arif <ariflucky1@gmail.com>
2026-09-12 21:56:29 +0530

    Hotfix:forgot password email tested postman

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import database from "../database/db.js";
-import bcypt from "bcrypt";
+import bcrypt from "bcrypt";
 import { sendToken } from "../utils/jwtToken.js";
 import { generateResetPasswordToken } from "../utils/generateResetPasswordToken.js";
 import { generateEmailTemplate } from "../utils/generateForgotPasswordEmailTemplate.js";
@@ -22,7 +22,7 @@ export const register = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("User already registered", 400));
   }
 
-  const hashedPassword = await bcypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
   const user = await database.query(
     "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
     [name, email, hashedPassword],
@@ -41,7 +41,7 @@ export const login = catchAsyncErrors(async (req, res, next) => {
   if (user.rows.length === 0) {
     return next(new ErrorHandler("Invalid email or password.", 401));
   }
-  const isPasswordMatch = await bcypt.compare(password, user.rows[0].password);
+  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
   if (!isPasswordMatch) {
     return next(new ErrorHandler("Invalid email or password.", 401));
   }
@@ -95,19 +95,19 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   try {
     await sendEmail({
       email: user.email,
-      subject: "Ecommerce password Recovery",
-      message,
+      subject: "Ecommerce Password Recovery",
+      message
     });
     res.status(200).json({
       success: true,
-      message: `Email sent to ${user.email} successfully.`,
+      message: `Email sent to ${user.email} successfully.`
     });
   } catch (error) {
     await database.query(
       `UPDATE users SET reset_password_token = NULL, reset_password_expire = NULL WHERE 
       email = $1`,
-      [email],
-    );
+      [email]
+    )
     return next(new ErrorHandler("Email could not be sent.", 500));
   }
 });
--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,9 +2,9 @@ import nodeMailer from 'nodemailer';
 
 export const sendEmail = async({ email, subject, message }) => {
     const transporter = nodeMailer.createTransport({
-        host: process.env.SMTP_HOST,
+        // host: process.env.SMTP_HOST,
         service: process.env.SMTP_SERVICE,
-        port: process.env.SMTP_PORT,
+        // port: process.env.SMTP_PORT,
         auth:{
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
