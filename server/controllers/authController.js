40769a5a2b48ac6ba6a7986df3c650d597ade839
Mohd Arif <ariflucky1@gmail.com>
2026-09-14 23:13:58 +0530

    reset passsword validations

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,12 +6,23 @@ import { sendToken } from "../utils/jwtToken.js";
 import { generateResetPasswordToken } from "../utils/generateResetPasswordToken.js";
 import { generateEmailTemplate } from "../utils/generateForgotPasswordEmailTemplate.js";
 import { sendEmail } from "../utils/sendEmail.js";
-import crypto from 'crypto'
+import crypto from "crypto";
 
 export const register = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
     return next(new ErrorHandler("Please provide all required fields.", 400));
+  }
+
+  if (
+    req.body.password.length < 8 ||
+    req.body.password.length > 16 ||
+    req.body.confirmPassword.length < 8 ||
+    req.body.confirmPassword.length > 16
+  ) {
+    return next(
+      new ErrorHandler("Password must be between 8 and 16 characters.", 400) 
+    );
   }
 
   const isAlreadyRegistered = await database.query(
@@ -75,7 +86,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   const { frontendUrl } = req.query;
   let userResult = await database.query(
     `SELECT * FROM users WHERE email = $1`,
-    [email]
+    [email],
   );
   if (userResult.rows.length === 0) {
     return next(new ErrorHandler("User not found with this email.", 404));
@@ -87,7 +98,7 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   await database.query(
     `UPDATE users SET reset_password_token = $1, reset_password_expire = to_timestamp($2)
     WHERE email = $3`,
-    [hashedToken, resetPasswordExpiretime / 1000, email]
+    [hashedToken, resetPasswordExpiretime / 1000, email],
   );
   const resetPasswordUrl = `${frontendUrl}/password/reset/${resetToken}`;
 
@@ -101,28 +112,43 @@ export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
     });
     res.status(200).json({
       success: true,
-      message: `Email sent to ${user.email} successfully.`
+      message: `Email sent to ${user.email} successfully.`,
     });
   } catch (error) {
     await database.query(
       `UPDATE users SET reset_password_token = NULL, reset_password_expire = NULL WHERE 
       email = $1`,
-      [email]
-    )
+      [email],
+    );
     return next(new ErrorHandler("Email could not be sent.", 500));
   }
 });
 
 export const resetPassword = catchAsyncErrors(async (req, res, next) => {
-  const {token} = req.params;
-  const resetPasswordToken = crypto.createHash("sha256").update(token).digest("hex");
-  const user = await database.query("SELECT * FROM user WHERE reset_password_token = $1 AND reset_password_expire > NOW()", 
-    [resetPasswordToken]
+  const { token } = req.params;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+  const user = await database.query(
+    "SELECT * FROM user WHERE reset_password_token = $1 AND reset_password_expire > NOW()",
+    [resetPasswordToken],
   );
-  if(user.rows.length === 0) {
+  if (user.rows.length === 0) {
     return next(new ErrorHandler("Invalid or expired reset token.", 400));
   }
-  if(req.body.password !== req.body.confirmPassword){
-    
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(new ErrorHandler("Password do not match.", 400));
+  }
+
+  if (
+    req.body.password.length < 8 ||
+    req.body.password.length > 16 ||
+    req.body.confirmPassword.length < 8 ||
+    req.body.confirmPassword.length > 16
+  ) {
+    return next(
+      new ErrorHandler("Password must be between 8 and 16 characters.", 400) 
+    );
   }
 });
