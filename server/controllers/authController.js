9ff00cf1ca4117226363813ec125b8c6586a7a8d
Mohd Arif <ariflucky1@gmail.com>
2026-09-15 20:52:32 +0530

    Update password api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -154,3 +154,33 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
   );
   sendToken(updatedUser.rows[0], 200, "Password reset successfully", res);
 });
+
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const {currentPassword, newPassword, confirmNewPassword} = req.body;
+  if(!currentPassword || !newPassword || !confirmNewPassword){
+    return next(new ErrorHandler("please provide all required fields.", 400))
+  }
+  const isPasswordMatch = await bcrypt.compare(currentPassword, req.user.password);
+  if(!isPasswordMatch){
+    return next(new ErrorHandler("Current password is incorrect.", 401));
+  }
+  if(newPassword !== confirmNewPassword){
+    return next(new ErrorHandler("New password do not match.", 400));
+  }
+
+  if (
+    newPassword.length < 8 ||
+    newPassword.length > 16 ||
+    confirmNewPassword.length < 8 ||
+    confirmNewPassword.length > 16
+  ) {
+    return next(
+      new ErrorHandler("Password must be between 8 and 16 characters.", 400),
+    );
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+  await database.query("UPDATE users SET password = $1 WHERE id = $2", [hashedPassword, req.user.id]);
+})
