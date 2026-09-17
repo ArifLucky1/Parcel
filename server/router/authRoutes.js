ece323bff31adbfbb339a27df8a8300a95aeaaa7
Mohd Arif <ariflucky1@gmail.com>
2026-09-17 23:14:07 +0530

    Update Profile route

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -7,6 +7,7 @@ import {
   register,
   resetPassword,
   updatePassword,
+  updateProfile,
 } from "../controllers/authcontroller.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
@@ -19,6 +20,7 @@ router.get("/me", isAuthenticated, getUser);
 router.post("/password/forgot", forgotPassword);
 router.put("/password/reset/:token", resetPassword);
 router.put("/password/update", isAuthenticated, updatePassword);
+router.put("/profile/update", isAuthenticated, updateProfile);
 
 
 export default router;
