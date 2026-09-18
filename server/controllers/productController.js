350d1bd08fe8d145390245534dbf86658505f6c0
Mohd Arif <ariflucky1@gmail.com>
2026-09-18 22:36:24 +0530

    Products API

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,7 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import database from "../database/db.js";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const { name, description, price, category, stock } = req.body;
@@ -8,29 +9,48 @@ export const createProduct = catchAsyncErrors(async (req, res, next) => {
 
   if (!name || !description || !price || !category || !stock) {
     return next(
-      new ErrorHandler("Please provide complete product details.", 400)
+      new ErrorHandler("Please provide complete product details.", 400),
     );
   }
 
-  let uploadedImage = [];
-  if(req.files && req.files.images){
+  let uploadedImages = [];
+  if (req.files && req.files.images) {
     const images = Array.isArray(req.files.images)
-    ? req.files.images : [req.files.images];
+      ? req.files.images
+      : [req.files.images];
 
-    for(const image of images){
-        const result = await cloudinary.uploader.upload(image.tempFilepath, {
-            folder: "Ecommerce_Product_Images",
-            width: 1000,
-            crop: "scale"
-        });
+    for (const image of images) {
+      const result = await cloudinary.uploader.upload(image.tempFilepath, {
+        folder: "Ecommerce_Product_Images",
+        width: 1000,
+        crop: "scale",
+      });
 
-
-        uploadedImage.push({
-            url: result.secure_url,
-            public_id: result.public_id
-        })
+      uploadedImages.push({
+        url: result.secure_url,
+        public_id: result.public_id,
+      });
     }
-  }  
+  }
 
-  
+  const product = await database.query(
+    `INSERT INTO products (name, description, price, category, stock, images, created_by)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+    [
+      name,
+      description,
+      price,
+      category,
+      stock,
+      JSON.stringify(uploadedImages),
+      created_by,
+    ],
+  );
+
+  res.status(201).json({
+    success: true,
+    message: "Product created successfully.",
+    product: product.rows[0]
+
+  })
 });
