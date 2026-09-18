9401149ded421c9e4aa10354e02d7189417fd8cf
Mohd Arif <ariflucky1@gmail.com>
2026-09-18 22:15:11 +0530

    Product error

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -2,5 +2,13 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
-    const {} = req.body;
-})
+  const { name, description, price, category, stock } = req.body;
+  const created_by = req.user.id;
+
+  if (!name || !description || !price || !category || !stock) {
+    return next(
+      new ErrorHandler("Please provide complete product details.", 400)
+    );
+  }
+  
+});
