fcd417fa0c66d6cc6d367f95b690e281e5042781
Mohd Arif <ariflucky1@gmail.com>
2026-09-18 22:24:47 +0530

    Product uploaded image

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,6 @@
 import { catchAsyncErrors } from "../middlewares/catchAsyncError.js";
 import ErrorHandler from "../middlewares/errorMiddleware.js";
+import {v2 as cloudinary} from "cloudinary";
 
 export const createProduct = catchAsyncErrors(async (req, res, next) => {
   const { name, description, price, category, stock } = req.body;
@@ -10,5 +11,18 @@ export const createProduct = catchAsyncErrors(async (req, res, next) => {
       new ErrorHandler("Please provide complete product details.", 400)
     );
   }
-  
+
+  let uploadedImage = [];
+  if(req.files && req.files.images){
+    const images = Array.isArray(req.files.images)
+    ? req.files.images : [req.files.images];
+
+    for(const image of images){
+        const result = await cloudinary.uploader.upload(image.tempFilepath, {
+            folder: "Ecommerce_Product_Images",
+            width: 1000,
+            crop: "scale"
+        });
+    }
+  }  
 });
